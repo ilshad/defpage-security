65d89be93c2995784febe7287e8b2f52b5f699e3
Ilshad Khabibullin <astoon@spacta.com>
2012-02-12 02:13:58 +0400

    "204 No Content" instead of "202 Accepted" receive when sending email via notify unit.

--- defpage/security/mail.py
+++ defpage/security/mail.py
@@ -9,7 +9,7 @@ def sendmail(recipients, subject, body):
     headers = {'Content-Type':'application/json'}
     h = httplib2.Http()
     response, _c = h.request(url, "POST", content, headers)
-    if response.status != 202:
+    if response.status != 204:
         raise ServiceCallError
 
 signup_message = u'<div style="color:#a52a2a; font-width:bold; font-size:20px;">Hello!</div>\n<p>Thank you for registering with <b>defpage.com!</b></p>\n\n<p>Verify Your Email Address Registration.\nPlease click on the confirmation link below to activate your account:\n%s</p>\n\n<p>After confirmation, use this email address and password <b><em>%s</em></b> to sign in.</p>\n\n<p>This email was sent to %s.</p>'
