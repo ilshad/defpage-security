a37f5f517b9e0ebfbe6cac12da7eeb36e9c5d264
Ilshad Khabibullin <astoon@spacta.com>
2012-09-21 23:46:11 +0400

    fix signup_message link

--- defpage/security/views.py
+++ defpage/security/views.py
@@ -67,7 +67,7 @@ def signup(req):
         pr = PendingRegistration(email)
         code = pr.code
         dbs.add(pr)
-        url = u"http://%s/signup_confirm?&email=%s&code=%s" % (req.host, email, code)
+        url = u"http://%s/signup_confirm?email=%s&code=%s" % (req.host, email, code)
         message_body = signup_message % (url, url, email)
         sendmail(recipients=[email], subject=u"Registration on defpage.com", body=message_body)
         req.session.flash(u"We've sent you a confirmation code! Please check your email.")
